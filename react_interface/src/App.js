71e5068c9af94f53366a4cd200b529755418b3f3
Dao Zheng <daoyzheng@gmail.com>
2019-05-04 14:26:08 -0700

    Add example json array object

--- react_interface/src/App.js
+++ react_interface/src/App.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import Todos from './components/Todos';
 
 class App extends Component {
+    state = {
+        todos: [
+            {
+                id: 1,
+                title: 'Take out the trash',
+                completed: true
+
+            },
+            {
+                id: 2,
+                title: 'Do app',
+                completed: false
+
+            },
+            {
+                id: 3,
+                title: 'Do frontend',
+                completed: false
+
+            }
+        ]
+    }
     render() {
+        console.log(this.state.todos)
         return (
             <div className="App">
                 <h1>Todo List</h1>
+                <Todos />
             </div>
         );
     }
